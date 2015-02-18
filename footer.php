a82375e04be2d30892f1c2f9ec6414c3a7211dc7
Anil Bhaila <anil.bhaila.2020@gmail.com>
2015-02-18 23:21:05 +0545

    navigation menu and google analytics tracking code

---
+++ footer.php
@@ -0,0 +1,16 @@
+
+<footer class="site-footer">
+    <p><?php bloginfo('name');?> - &copy;<?php echo date('Y');?></p>
+
+    <nav class="site-nav">
+        <?php $args = array(
+            'theme_location' => 'footer'
+        );?>
+        <?php wp_nav_menu( $args );?>
+    </nav>
+</footer>
+
+</div> <!-- /container -->
+<?php wp_footer();?>
+</body>
+</html>
