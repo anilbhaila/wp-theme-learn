d1f7d1a46e837f91590465d4765560ab5ab05b3d
Anil Bhaila <anil.bhaila.2020@gmail.com>
2015-02-23 22:40:15 +0545

    wordpress post meta data, date, author, category and links

--- index.php
+++ index.php
@@ -12,7 +12,20 @@ if (have_posts()):
     while (have_posts()): the_post(); ?>
         <article class="post">
             <h2><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h2>
+            <p class="post-info"><?php the_time('F jS, Y');?> | by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID'))?>"><?php the_author();?></a> | Posted in
+            <?php
+            $categories = get_the_category();
+            $separator = ", ";
+            $output = '';
 
+            if($categories){
+                foreach ($categories as $category){
+                    $output .= '<a href="'.get_category_link($category->term_ID).'">'.$category->cat_name . $separator.'</a>';
+                }
+                echo trim($output, $separator);
+            }
+            ?>
+            </p>
             <?php the_content(); ?>
         </article>
     <?php
